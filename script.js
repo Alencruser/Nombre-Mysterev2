1c0ebb25822d76b4db2dbce09b1c1df0e3168d37
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-04 21:13:15 +0200

    exo fini sans package de random number Seule faille : Le serveur doit etre restart pour avoir un autre nombre

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
 var express = require('express');
-var body= require('body-parser')
+var bb= require('express-busboy')
 
 var app = express();
 var x = [Date.now()];
 x = x.toString().split('');
 x=x[x.length-2]
 
+var i=3;
+var v='Pas encore de resultat';
 app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
- 	res.write('<!DOCTYPE html>'+
- 		'<head>'+
- 		'<meta charset="utf-8"/>'+
- 		'<title>Nombre mystère</title>'+
- 		'</head>'+
- 		'<body>'+
- 		'<input id="number" type="number" min="0" max="9">'+
- 		'<button id="ans">Tester</button>'+
- 		x+
- 		'</body>')
- 	res.end()
-});
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<!DOCTYPE html>'+
+		'<head>'+
+		'<meta charset="utf-8"/>'+
+		'<title>Nombre mystère</title>'+
+		'</head>'+
+		'<body>'+
+		'<form method="post" action="/">'+
+		'<input type="number" min="0" max="9" name="number">'+
+		'<input type="submit" value="Valider">'+
+		x+
+		'</form>'+
+		i+'essais restants'+'<br />'+
+		v+
+		'</body>');
+ });
+
+bb.extend(app)
 
 app.post('/',function(req,res){
-	var a=req.body.ans.value;
-	a.addEventListener('click',function(){
-		var b=req.body.number.value;
-	if(b==x){
-		console.log('Trouvé ! Bien joué à toi !')
-	}if(b<x){
-		console.log('C\'est plus')
-	}if(b>x){
-		console.log('C\'est moins !')
+
+	function retrieve(){
+		var b=req.body.number;
+		if(b==x){
+			v='Trouvé ! Bien joué à toi !'
+			i=3
+		}if(b<x){
+			i--
+			v='C\'est plus'
+		}if(b>x){
+			i--
+			v='C\'est moins !'
+		}
+		if(i==0){
+			v='Vous avez perdu'
+			i=3
+		}
+		res.redirect('/')
 	}
-})
-	res.end()
-})
+	retrieve();
+});
 
 app.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !');
+	res.setHeader('Content-Type', 'text/plain');
+	res.status(404).send('Page introuvable !');
 });
 
 app.listen(8080);
